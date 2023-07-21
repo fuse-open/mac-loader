1a7fc7ae22184856bc7d022d92343e49e63b1f29
Morten Daniel Fornes <mdfornes@gmail.com>
2023-07-21 20:11:08 +0200

    Set DllImportResolver for loading native libraries

--- src/UnoAppLoader.cs
+++ src/UnoAppLoader.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.IO;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using Claunia.PropertyList;
+using ObjCRuntime;
 
 namespace Uno.AppLoader
 {
@@ -32,6 +34,38 @@ namespace Uno.AppLoader
 
         static Application LoadUnoApp(string assemblyPath, string mainClass)
         {
+            // Set DllImportResolver for loading native libraries used in generated assemblies
+            AppDomain.CurrentDomain.AssemblyLoad += (sender, ev) => {
+                var dir1 = Path.GetDirectoryName(ev.LoadedAssembly.Location);
+                var dir2 = Path.GetDirectoryName(assemblyPath);
+
+                // Skip assemblies not included in the build output
+                if (dir1 != dir2)
+                    return;
+
+                try
+                {
+                    NativeLibrary.SetDllImportResolver(ev.LoadedAssembly, (libraryName, assembly, searchPath) => {
+                        if (!libraryName.Contains(".dll") || libraryName.StartsWith("/"))
+                            return IntPtr.Zero;
+
+                        var dir = Path.GetDirectoryName(assembly.Location);
+                        var lib = Path.Combine(dir, "lib" + libraryName.Replace(".dll", ".dylib"));
+
+                        if (File.Exists(lib))
+                            return Dlfcn.dlopen(lib, 0);
+
+                        Console.Error.WriteLine("Native library not found: " + lib);
+                        return IntPtr.Zero;
+                    });
+                }
+                catch (Exception e)
+                {
+                    Console.Error.WriteLine("NativeLibrary.SetDllImportResolver: " + e);
+                }
+            };
+
+            // Load the main generated assembly
             var assembly = Assembly.LoadFile(assemblyPath);
             var type = assembly.GetType(mainClass, true);
             return (Application)Activator.CreateInstance(type);
